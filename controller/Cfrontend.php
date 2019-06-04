86807ac894dba30610e9d370f9ad79f2275cda8e
Alexis Gautier <alexisxgautier@gmail.com>
2019-06-04 14:21:41 +0200

    [1] Various codes tryed to make the redirect works properly but none working yet. Last commit SHA : 2c76319b98fa03e19fa52338247e0525b5b88148

--- controller/Cfrontend.php
+++ controller/Cfrontend.php
@@ -37,4 +37,35 @@ function deletePost($postId)
 {
     $postDelete = erasePost($postId);
     header('Location: ' . $_SERVER['HTTP_REFERER']); //FIXME problem = when delete button used on a specific post (BE), sends back to post that has just been deleted. + not good to use ? see : https://stackoverflow.com/questions/5285031/back-to-previous-page-with-header-location-in-php
+
+
+//try 3 : not working
+    
+    // if($_GET['id'] !="")
+    // {
+    // header('Location: index.php');
+    // }
+    // else
+    // {
+    // header('Location: ' . $_SERVER['HTTP_REFERER']);
+    // } 
+
+//try 2 : not working
+    // if('Location: index.php' OR 'Location: index.php?action=listPosts' ){ // FIXME (if elseif not working) problem = when delete button used on Admin page (BE), sends back to FE home page instead of BE Adminpage
+    //     header('Location: index.php?action=listPosts'); 
+    // }
+    // elseif('Location: index.php?action=listostsAdmin'){
+    //     header('Location: index.php?action=listPostsAdmin');
+    // }
+    
+
+//try 1 : not working
+    // //tryed the following code bug doesn't work because it brings conflicts between actions related to $_GET['action] defined here and in router index.php
+    // // if($_GET['action'] == '' OR $_GET['action'] == 'listPosts'){ // FIXME (if elseif not working) problem = when delete button used on Admin page (BE), sends back to FE home page instead of BE Adminpage
+    // //     header('Location: index.php?action=listPosts'); 
+    // // }
+    // // elseif($_GET['action'] == 'listPostsAdmin'){
+    // //     header('Location: index.php?action=listPostsAdmin');
+    // // }
+
 } 
