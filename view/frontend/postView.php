61a324a9404ef48b38196291d64f7495908b4fc3
Alexis Gautier <alexisxgautier@gmail.com>
2019-06-04 21:02:40 +0200

    [1] - 'Manage Comments' feature new update working : display report button only if comment not already reported in FE

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -54,7 +54,13 @@
         <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['mod_comment_date'] ?></p>
         <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
 
-        <button class="userBtns"><a href="index.php?action=reportComment&amp;id=<?= $post['id'] ?>&amp;commentId=<?= $comment['id'] ?>"  onclick="return alert('Commentaire signalé')" >Signaler</a></button>
+    <?php 
+    //display report button only if comment not already reported
+    if($comment['flag'] == 0)
+    {
+        echo '<button class="userBtns"><a href="index.php?action=reportComment&amp;id=' . $post['id'] . '&amp;commentId='. $comment['id'] . '"  onclick="return alert(\'Commentaire signalé\')" >Signaler</a></button>' ;
+    }   
+    ?>    
 
         <!-- gets the commentId as a parameter in the URL of the comment to delete AND the post id to return on the same post after comment has been deleted-->
         <button class="adminBtns"><a href="index.php?action=deleteComment&amp;id=<?= $post['id'] ?>&amp;commentId=<?= $comment['id'] ?>" onclick="return confirm('Etes vous sûr?')">Supprimer</a></button>
