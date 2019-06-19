d850bcd1d7545856ffbcb77c70f2f255b8e39cbf
Alexis Gautier <alexisxgautier@gmail.com>
2019-06-19 11:18:00 +0200

    TinyMCE default language sets to french

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -13,7 +13,9 @@
         <script src="https://cdn.tiny.cloud/1/mpujcznv2qarii6l81l67tjf7m8okalduchv3ot3xy9hv0g3/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
         <script type="text/javascript">
         tinymce.init({
-        selector: '#postContent'
+        selector: '#postContent',
+        language_url : 'vendor/tinymce/langs/fr_FR.js',
+        language: 'fr_FR'
         });
         </script>
         
