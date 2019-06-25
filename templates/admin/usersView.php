dcdad977f9e07727edf74695dbe99fcb94ccb8a9
Alexis Gautier <alexisxgautier@gmail.com>
2019-06-25 22:36:48 +0200

    User Page desing done

--- templates/admin/usersView.php
+++ templates/admin/usersView.php
@@ -1,68 +1,74 @@
 <?php $title = 'Gestion des utilisateurs'; ?>
 
 <?php ob_start(); ?>
-<?php require('templates/pagination.php'); ?>
-<p>Afficher par <button><a href="index.php?action=manageUsers&page=<?= $_GET['page'] ?>&sortBy=10">10</a></button> <button><a href="index.php?action=manageUsers&page=<?= $_GET['page'] ?>&sortBy=20">20</a></button> <button><a href="index.php?action=manageUsers&page=<?= $_GET['page'] ?>&sortBy=99999999999999999999">Tous</a></button></p>     
 
-<h2>Utilisateurs</h2>
-<form action="index.php?action=manageAllSelectedUsers" method="post"> 
-        <input type="checkbox" id="checkAllUsers" >
-        <label for="checkAllUsers"> Tout sélectionner / désélectionner </label>
-        <input type="submit" name="deleteSelectedUsers[]" value="Supprimer" onclick="return confirm('Etes-vous sûr?')">
+<div class="container">
+<h2>Gestion des utilisateurs</h2>
+
+            <?php require('templates/pagination.php'); ?>
+            <p>Afficher par 
+            <a href="index.php?action=manageUsers&page=<?= $_GET['page'] ?>&sortBy=10"><button class="btn btn-info btn-sm"><strong>10</strong></button></a> 
+            <a href="index.php?action=manageUsers&page=<?= $_GET['page'] ?>&sortBy=20"><button class="btn btn-info btn-sm"><strong>20</strong></button></a> 
+            <a href="index.php?action=manageUsers&page=<?= $_GET['page'] ?>&sortBy=99999999999999999999"><button class="btn btn-info btn-sm"><strong>Tous</strong></button></a> 
 
 
-<?php
-    //je déclare un tableau vide qui va me servir a stocker tous les ids des utilisateurs
-    $arrayUsersIDs = array();
- 
-if(!empty($allUsers)) //needed otherwise gives an error on the usersView.php when no users in DB
-{    
-    for ($i = 0 ; $i < sizeof($allUsers) ; $i++)
-    {
-        $userid = $allUsers[$i]->id(); 
-        $username = $allUsers[$i]->username();
-        $email = $allUsers[$i]->email();
-        $registrationDate = $allUsers[$i]->modRegistrationDate();
-        $group = $allUsers[$i]->groupId();
+            <form action="index.php?action=manageAllSelectedUsers" method="post"> 
+                    <input type="checkbox" id="checkAllUsers" >
+                    <label for="checkAllUsers"> Tout sélectionner / désélectionner </label>
+                    <input class="btn btn-danger btn-sm" type="submit" name="deleteSelectedUsers[]" value="Supprimer" onclick="return confirm('Etes-vous sûr?')">
 
-    ?>
-        <div class="usersDisplay">
-                <p class=""><strong><?= htmlspecialchars($username) ?></strong> <?= $email ?>
-                        enregistré le <strong> <?= $registrationDate ?></strong> 
-                
-                        <p>role : 
-                        <?php 
-                        if( $group == 0)
-                            { 
-                                echo  'Utilisateur';
-                                ?>
-                                <button class="adminBtns"><a href="index.php?action=updateRole&amp;userID=<?= $userid ?>&amp;role=1" onclick="return confirm('Etes-vous sûr?')">Passer en Admin</a></button>
-                                <?php
-                                
-                            }
-                        elseif( $group == 1)
-                        { 
-                            echo '<strong>Admin</strong>';
-                            ?>
-                            <button class="adminBtns"><a href="index.php?action=updateRole&amp;userID=<?= $userid ?>&amp;role=0" onclick="return confirm('Etes-vous sûr?')">Passer en Utilisateur</a></button>
-                            <?php
-                        }      
-                        ?></p>
 
-                
-                <input type="checkbox" id="userID" name="selectUsers[]" value="<?= $userid?>" >
+            <?php
+                //This array is used to stock every users'Ids
+                $arrayUsersIDs = array();
+            
+            if(!empty($allUsers)) //needed otherwise gives an error on the usersView.php when no users in DB
+            {    
+                for ($i = 0 ; $i < sizeof($allUsers) ; $i++)
+                {
+                    $userid = $allUsers[$i]->id(); 
+                    $username = $allUsers[$i]->username();
+                    $email = $allUsers[$i]->email();
+                    $registrationDate = $allUsers[$i]->modRegistrationDate();
+                    $group = $allUsers[$i]->groupId();
 
-                <button class="adminBtns"><a href="index.php?action=deleteUser&amp;userID=<?= $userid ?>" onclick="return confirm('Etes-vous sûr?')">Supprimer</a></button>
-        </div>
-    <?php
-        
-        //pour chaque utilisateur, je rajoute son id dans le tableau $arrayUsersIDs
-        array_push($arrayUsersIDs, $userid);    
-    }
-}    
-    ?>    
-</form>
+                ?>
+                    <div class="usersDisplay">
+                            <p class="userListHeader"><input type="checkbox" id="userID" name="selectUsers[]" value="<?= $userid?>" ><strong><?= htmlspecialchars($username) ?></strong>&emsp;-&emsp;<?= $email ?>&emsp;-&emsp;enregistré le <strong> <?= $registrationDate ?></strong></p> 
 
+                                <div class="roleAndDelete">
+                                    <p ><a class="deleteUserLink" href="index.php?action=deleteUser&amp;userID=<?= $userid ?>" onclick="return confirm('Etes-vous sûr?')"><i class="fas fa-user-times"></i>Supprimer</a>    
+                                    <p><strong>Role :</strong> 
+                                    <?php 
+                                    if( $group == 0)
+                                        { 
+                                            echo  'Utilisateur';
+                                            ?>
+                                            <button class="btn btn-warning btn-sm"><a href="index.php?action=updateRole&amp;userID=<?= $userid ?>&amp;role=1" onclick="return confirm('Etes-vous sûr?')">Passer en Admin</a></button>
+                                            <?php
+                                            
+                                        }
+                                    elseif( $group == 1)
+                                    { 
+                                        echo '<strong style="color:red">Admin</strong>';
+                                        ?>
+                                        <button class="btn btn-warning btn-sm"><a href="index.php?action=updateRole&amp;userID=<?= $userid ?>&amp;role=0" onclick="return confirm('Etes-vous sûr?')">Passer en Utilisateur</a></button>
+                                        <?php
+                                    }      
+                                    ?></p>
+                                </div>    
+
+                                    
+                    </div>
+                <?php
+                    
+                    //pour chaque utilisateur, je rajoute son id dans le tableau $arrayUsersIDs
+                    array_push($arrayUsersIDs, $userid);    
+                }
+            }    
+                ?>    
+            </form>
+</div>
 <!-- < ?php var_dump($allUsers); ?> FIXME : erase me -->
 
     <!-- Select / Deselect all checkboxes (for Reported comments)  -->   
