09b6676b6fea0e8e73d6f4885b769bf80c745f60
Alexis Gautier <alexisxgautier@gmail.com>
2019-06-30 18:26:16 +0200

    error message when database connection failed less explicit

--- src/entity/Manager.php
+++ src/entity/Manager.php
@@ -11,7 +11,13 @@ class Manager
     // General protected function to connect to database
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=p4blog;charset=utf8', 'root', '');
-        $this->_db= $db;
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=p4blog;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $this->_db= $db;
+        }
+        catch(Exception $e) {
+            throw new Exception('La connexion à la base de donnée a échouée.');
+        }
+
     }
 }
